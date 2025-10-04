31667d428cd65f39c9ba2c40809b2eb5f7ed8e1d
Sumi Tharayil Surendran <tharayilsurendransumi@gmail.com>
2025-10-04 20:48:34 +0100

    Swapping two numbers

--- src/main/java/SwappingTwoNumbers.java
+++ src/main/java/SwappingTwoNumbers.java
@@ -8,5 +8,9 @@ public class SwappingTwoNumbers {
         System.out.println("Enter the second number");
         int second_number = scanner.nextInt();
         System.out.println("Before swapping,first number"+first_number+"second number"+second_number);
+        first_number = first_number+second_number;
+        second_number =first_number-second_number;
+        first_number = first_number-second_number;
+        System.out.println("After swapping,first number"+first_number+"second number"+second_number);
     }
 }
