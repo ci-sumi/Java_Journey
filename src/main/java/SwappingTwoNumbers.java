cd9cb1a8bd6a5b8005d7bc730f0860eef3426ac0
Sumi Tharayil Surendran <tharayilsurendransumi@gmail.com>
2025-10-04 20:46:06 +0100

    Initial commit

---
+++ src/main/java/SwappingTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SwappingTwoNumbers {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number");
+        int first_number = scanner.nextInt();
+        System.out.println("Enter the second number");
+        int second_number = scanner.nextInt();
+        System.out.println("Before swapping,first number"+first_number+"second number"+second_number);
+    }
+}
