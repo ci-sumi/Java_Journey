47b5bb5826ae15e6ce86673833f4eab16e42b565
Sumi Tharayil Surendran <tharayilsurendransumi@gmail.com>
2025-10-05 06:19:26 +0100

    Add shopping cart program

---
+++ src/main/java/ShoppingCart.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ShoppingCart {
+    public static void main(String[] args){
+        System.out.println("*****SHOPPING CART*****");
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        System.out.println("What you like to buy:?");
+        String item = scanner.nextLine();
+        System.out.println("Whats the price for each:?");
+        double price = scanner.nextDouble();
+        System.out.println("How many would you like to buy:?");
+        int quantity = scanner.nextInt();
+        System.out.println("You have bought:"+quantity+""+item+"/s");
+        double total = quantity * price;
+        System.out.println("Your total is:"+total);
+        scanner.close();
+
+
+
+
+
+
+    }
+}
