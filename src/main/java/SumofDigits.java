feb42c001c65042936c791196f84873f9cea11bb
Sumi Tharayil Surendran <tharayilsurendransumi@gmail.com>
2025-10-06 10:43:33 +0100

    Sum of digits

--- src/main/java/SumofDigits.java
+++ src/main/java/SumofDigits.java
@@ -1,2 +1,22 @@
+import java.util.*;
 public class SumofDigits {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int userInput = scanner.nextInt();
+        int result = sumofDigits(userInput);
+        System.out.println(result);
+
+
+    }
+    public static int sumofDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            number = number / 10;
+            sum = sum + digit;
+
+        }return sum;
+
+    }
 }
