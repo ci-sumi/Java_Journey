7d6797ae65568de269f3268ddd772c289f6fcdaa
Sumi Tharayil Surendran <tharayilsurendransumi@gmail.com>
2025-10-06 11:22:29 +0100

    Reverse NUmber

---
+++ src/main/java/ReverseNumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ReverseNumber {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int userInput = scanner.nextInt();
+        int result = reverseNumber(userInput);
+        System.out.println("The reversed number is:"+result);
+
+    }
+    public static int reverseNumber(int number){
+        int revese=0;
+        while(number>0){
+            int reversed = number%10;
+            number = number/10;
+            revese = revese*10+reversed;
+
+        }return revese;
+    }
+}
